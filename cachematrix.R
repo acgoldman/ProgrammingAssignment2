3ad737306ac046f495e9ddfdd9ad52e5a98faa1c
acgoldman <ahmgoldman@hotmail.com>
2018-06-30 10:56:40 -0500

    Coded required functions
    Coded caching matrix and solving for it or using cached solution, when found.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a matrix cached for use in multiple seperate calls
+## By using this function, you can cache the inverse of the matrix
+## this function, makeCacheMatrix merely sets the matrix and stores it in
+## the calling environment rather than the function environment
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  i <- NULL
+  set <- function(y=matrix()){
+    myCMatrix <<- y
+    print(myCMatrix)
+    i <<- NULL
+  }
+  get <- function() myCMatrix
+  setinverse <- function(inv) i <<- inv
+  getinverse <- function() i
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This matrix solves (calculates the inverse) of the given matrix, unless already done, in which case it returns its cached solution
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  invrs <- x$getinverse()
+  if(!is.null(invrs)) {
+    message("getting cached data")
+    return(invrs)
+  }
+  data <- x$get()
+  invrs <- solve(data, ...)
+  x$setinverse(invrs)
+  invrs
 }
